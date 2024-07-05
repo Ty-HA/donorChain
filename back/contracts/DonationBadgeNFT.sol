ae9f91972e2820ad2c1e83f82aac64979a4a326b
Ty HA <bichty.ha@gmail.com>
2024-07-05 15:41:43 +0200

    DonationBadgeNFT comments

--- back/contracts/DonationBadgeNFT.sol
+++ back/contracts/DonationBadgeNFT.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.24;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
+/// @title DonationBadgeNFT
+/// @author Ty HA
+/// @notice This contract is used to mint tokens representing donation badges as rewarding for donors
 contract DonationBadgeNFT is ERC721, Ownable {
     uint256 private _nextTokenId;
 
@@ -44,55 +48,74 @@ contract DonationBadgeNFT is ERC721, Ownable {
         _;
     }
 
+    /// @notice Set the address of the donation contract
+    /// @param _donationContract  The address of the donation contract
     function setDonationContract(address _donationContract) external onlyOwner {
         donationContract = _donationContract;
     }
 
-    function mintBadge(address donor, uint256 totalDonated) external onlyDonationContract returns (uint256) {
-        Tier newTier = getTierForAmount(totalDonated);
-        require(newTier > donorHighestTier[donor], "Donor already has this tier or higher");
+    /// @notice Mint a badge for a donor
+    /// @param _donor  The address of the donor
+    /// @param _totalDonated  The total amount donated by the donor
+    /// @return The token ID of the minted badge
+    function mintBadge(address _donor, uint256 _totalDonated) external onlyDonationContract returns (uint256) {
+        Tier newTier = getTierForAmount(_totalDonated);
+        require(newTier > donorHighestTier[_donor], "Donor already has this tier or higher");
 
         uint256 newTokenId = _nextTokenId++;
-        _safeMint(donor, newTokenId);
+        _safeMint(_donor, newTokenId);
         badges[newTokenId] = Badge(newTier, block.timestamp);
-        donorHighestTier[donor] = newTier;
+        donorHighestTier[_donor] = newTier;
 
-        emit BadgeMinted(donor, newTokenId, newTier);
+        emit BadgeMinted(_donor, newTokenId, newTier);
 
         return newTokenId;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return tierURIs[badges[tokenId].tier];
+    /// @notice Get the URI of a badge token
+    /// @param _tokenId  The token ID of the badge
+    /// @return The URI of the badge token
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return tierURIs[badges[_tokenId].tier];
     }
 
     // ::::::::::::: SETTERS ::::::::::::: //
-    function setTierURI(Tier tier, string memory uri) external onlyOwner {
-        tierURIs[tier] = uri;
-        emit TierURIUpdated(tier, uri);
+
+    /// @notice Set the URI for a tier
+    /// @param _tier  The tier for which to set the URI
+    /// @param _uri  The new URI for the tier
+    function setTierURI(Tier _tier, string memory _uri) external onlyOwner {
+        tierURIs[_tier] = _uri;
+        emit TierURIUpdated(_tier, _uri);
     }
 
     // ::::::::::::: GETTERS ::::::::::::: //
 
-    function getTierForAmount(uint256 amount) public pure returns (Tier) {
-        if (amount >= GOLD_THRESHOLD) {
+    /// @notice Get the tier for a donation amount
+    /// @param _amount  The donation amount
+    /// @return The tier corresponding to the donation amount
+    function getTierForAmount(uint256 _amount) public pure returns (Tier) {
+        if (_amount >= GOLD_THRESHOLD) {
             return Tier.Gold;
-        } else if (amount >= SILVER_THRESHOLD) {
+        } else if (_amount >= SILVER_THRESHOLD) {
             return Tier.Silver;
-        } else if (amount >= BRONZE_THRESHOLD) {
+        } else if (_amount >= BRONZE_THRESHOLD) {
             return Tier.Bronze;
         } else {
             return Tier.None;
         }
     }
 
-    function getDonorBadges(address donor) external view returns (uint256[] memory) {
-        uint256 balance = balanceOf(donor);
+    /// @notice Get the badges owned by a donor
+    /// @param _donor  The address of the donor
+    /// @return An array of token IDs representing the badges owned by the donor
+    function getDonorBadges(address _donor) external view returns (uint256[] memory) {
+        uint256 balance = balanceOf(_donor);
         uint256[] memory tokenIds = new uint256[](balance);
         uint256 counter = 0;
         for (uint256 i = 0; i < _nextTokenId; i++) {
-            if (_exists(i) && ownerOf(i) == donor) {
+            if (_exists(i) && ownerOf(i) == _donor) {
                 tokenIds[counter] = i;
                 counter++;
             }
@@ -100,24 +123,32 @@ contract DonationBadgeNFT is ERC721, Ownable {
         return tokenIds;
     }
 
-    function getBadgeDetails(uint256 tokenId) external view returns (Tier tier, uint256 timestamp) {
-        require(_exists(tokenId), "Badge does not exist");
-        Badge memory badge = badges[tokenId];
+    /// @notice Get the details of a badge
+    /// @param _tokenId  The token ID of the badge
+    function getBadgeDetails(uint256 _tokenId) external view returns (Tier tier, uint256 timestamp) {
+        require(_exists(_tokenId), "Badge does not exist");
+        Badge memory badge = badges[_tokenId];
         return (badge.tier, badge.timestamp);
     }
 
-    function getDonorHighestTier(address donor) external view returns (Tier) {
-        return donorHighestTier[donor];
+    /// @notice Get the highest tier owned by a donor
+    /// @param _donor  The address of the donor
+    /// @return The highest tier owned by the donor
+    function getDonorHighestTier(address _donor) external view returns (Tier) {
+        return donorHighestTier[_donor];
     }
 
-    function getTierName(Tier tier) public pure returns (string memory) {
-        if (tier == Tier.Gold) return "Gold";
-        if (tier == Tier.Silver) return "Silver";
-        if (tier == Tier.Bronze) return "Bronze";
+    /// @notice Get the name of a tier
+    /// @param _tier  The tier
+    /// @return The name of the tier
+    function getTierName(Tier _tier) public pure returns (string memory) {
+        if (_tier == Tier.Gold) return "Gold";
+        if (_tier == Tier.Silver) return "Silver";
+        if (_tier == Tier.Bronze) return "Bronze";
         return "None";
     }
 
-    function _exists(uint256 tokenId) internal view returns (bool) {
-        return tokenId < _nextTokenId && tokenId != 0;
+    function _exists(uint256 _tokenId) internal view returns (bool) {
+        return _tokenId < _nextTokenId && _tokenId != 0;
     }
 }
