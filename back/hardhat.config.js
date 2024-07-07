5e693539cd24ef74c37f83a94aff76946695b297
Ty HA <bichty.ha@gmail.com>
2024-07-07 20:07:41 +0200

    update deploy script and deplyed to Arbitrum Sepolia

--- back/hardhat.config.js
+++ back/hardhat.config.js
@@ -2,9 +2,9 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
 require('dotenv').config()
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const ARBITRUM_SEPOLIA_RPC_URL = process.env.ARBITRUM_SEPOLIA_RPC_URL || "";
 const PK = process.env.PRIVATE_KEY || "";
-const ETHERSCAN = process.env.ETHERSCAN_API_KEY || "";
+const ARBISCAN = process.env.ARBISCAN_API_KEY || "";
 
 module.exports = {
   solidity: "0.8.24",
@@ -18,10 +18,10 @@ module.exports = {
     },
   },
   networks: {
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
+    arbitrumSepolia: {
+      url: ARBITRUM_SEPOLIA_RPC_URL,
       accounts: [`0x${PK}`],
-      chainId: 11155111
+      chainId: 421614
     },
     localhost: {
       url: "http://127.0.0.1:8545",
@@ -29,6 +29,6 @@ module.exports = {
     }
   },
   etherscan: {
-    apiKey: ETHERSCAN
+    apiKey: ARBISCAN
   },
 };
